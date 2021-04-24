063a577e90b00cbcd8deba0791af318f5028f823
ChaBls <charline.builles@hotmail.fr>
2021-04-24 18:54:02 +0200

    class 'Product' created, with 'product' objects attributes

---
+++ product.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:Utf8
+
+
+class Product:
+    def __init__(self,id=None,name=None,nutriscore=None,url=None):
+        self.id=name
+        self.name=name
+        self.nutriscore=nutriscore
+        self.url=url
+
