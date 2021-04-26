7a0b5f76b93116ea440beb50631e38131ddef35c
ChaBls <charline.builles@hotmail.fr>
2021-04-26 17:04:23 +0200

    for loop changed, 'self.saved_products_list' erased

--- manager.py
+++ manager.py
@@ -10,7 +10,6 @@ class Manager:
 
     def __init__(self):
         self.saved_categories=[]
-        self.saved_products_list=[]
         self.products_objects=[]
 
     def popular_categories(self):
@@ -22,31 +21,21 @@ class Manager:
             self.saved_categories.append(category['name'])
             print(category['name'])
 
-    def ten_products(self, CATEGORIES_NAMES):
+    def ten_products(self):
         '''Chaque nom de catégorie est sauvegardé dans les constantes, dans la liste 'CATEGORIES_NAME'
         Dans une boucle 'for', pour chaque nom contenu dans cette liste:
         Une requête est faite à partir d'un URL, grâce au module 'requests'. Les accolades contenues dans le lien sont remplacées par le nom de la catégorie.
-        La liste obtenue est encodée puis enregistrée dans la variable 'results'
-        Dans une boucle 'for', pour chaque dictionnaire de la liste sauvegardée précédemment, un objet issu de la classe 'Product' est créé.
+        La liste obtenue est encodée puis enregistrée dans la variable 'results'.
+        Une liste de dictionnaire issue de la requête est enregistrée dans 'products_list'.
+        Dans une boucle 'for', pour chaque dictionnaire de la liste, un objet issu de la classe 'Product' est créé.
         Les attributs de ces objets sont obtenus à partir des paires clef/valeur du dictionnaire.
         '''
         for category in CATEGORIES_NAMES:
             request = requests.get('https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0= {} &json=1'.format(category))
             results = request.json()
             products_list = results['products'] # Liste de dictionnaires
-            self.saved_products_list.append(products_list)  # Liste enregistrée dans la liste 'saved_products_list'
-            for liste in self.saved_products_list: # Pour chaque liste dans la liste 'saved.products.list'
-                for product in liste[:10]:  # Pour chaque dictionnaire de la liste 'products_list'
-                    # Créer un objet Product()
-                    self.products_objects.append(Product(id=product['id'],name=product['product_name'],nutriscore=product['nutriscore_grade'],url=product['url']))
-                    # Afficher les ojects créés
-                    print(self.products_objects)
+            for product in products_list[:10]: # Pour chaque dictionnaire de la liste
+                self.products_objects.append(Product(name=product['product_name'],nutriscore=product['nutriscore_grade'],url=product['url']))
 
-                ''' Pour chaque objet dans la liste d'objets
-                Afficher le nom
-                Afficher l'id
-                '''
-                for objects in self.products_objects:
-                    print(objects.name)
-                    print(objects.id)
-
+            for objects in self.products_objects:
+                print(objects.nutriscore)
