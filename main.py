bfb25fc005a7f4bf9d7a9f0d72b4879ff557fe4a
ChaBls <charline.builles@hotmail.fr>
2021-05-29 16:54:03 +0200

    comment changed

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:Utf8
-from category_manager import CategoryManager
-from product_manager import ProductManager
 from api_manager import APIManager
 
 
-manager_object=APIManager()
+# APIManager sending_products() method is called, after APIManager object is created.
 
-# We ask the program to create 10 objects from Product(), based on the categories we've saved with the 1st method.
-manager_object.sending_products(manager_object.ten_products(manager_object.popular_categories()))
+manager_object=APIManager()
+manager_object.sending_products()
 
