9af4211b3f2714f39d8b0316e81c806f478f0255
ChaBls <charline.builles@hotmail.fr>
2021-05-29 16:55:34 +0200

    generate_categories method

--- product.py
+++ product.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:Utf8
-
+from requests import api
+from category import Category
 
 class Product:
-    def __init__(self,id=None,name=None,nutriscore=None,url=None):
-        self.id=name
+    def __init__(self,name,nutriscore,url,categories,id=None):
+        self.id=id
         self.name=name
         self.nutriscore=nutriscore
         self.url=url
+        self.categories=categories
+        if type(categories) is str:
+            self.generate_categories()
+
+    def generate_categories(self):
+        categories_list = self.categories.split(",")
+        self.categories = [Category(x) for x in categories_list]
+
+    def __str__(self):
+        return self.name
 
