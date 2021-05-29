1d55d455b835f7130b5a8710d0cca2f9c99c4040
ChaBls <charline.builles@hotmail.fr>
2021-05-29 16:56:25 +0200

    connection programto database edited

--- product_manager.py
+++ product_manager.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:Utf8
+import mysql.connector
 
 
 class ProductManager:
     
-    def save(self):
-        print("it's done!")
+    def save(self, products):
+        # The method allows the program to be connected to MariaDB database.
+
+        for product in products:
+            data_base = mysql.connector.connect(host='localhost',user='chanelire',password='Baudelaire666!',database='OpenFoodFacts')
+            cursor = data_base.cursor() # Allows to iterate through the database
+            cursor.execute("INSERT INTO products(name,nutriscore,url) VALUES (product.name,product.nutriscore,product.url")
+            data_base.commit()
+            data_base.close()
 
