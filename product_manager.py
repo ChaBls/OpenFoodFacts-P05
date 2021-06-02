848ca1c0d4c84495ca1b79652da0b561c281b9dd
ChaBls <charline.builles@hotmail.fr>
2021-06-02 14:08:45 +0200

    save method modified

--- product_manager.py
+++ product_manager.py
@@ -8,10 +8,11 @@ class ProductManager:
     def save(self, products):
         # The method allows the program to be connected to MariaDB database.
 
-        for product in products:
-            data_base = mysql.connector.connect(host='localhost',user='chanelire',password='Baudelaire666!',database='OpenFoodFacts')
-            cursor = data_base.cursor() # Allows to iterate through the database
-            cursor.execute("INSERT INTO products(name,nutriscore,url) VALUES (product.name,product.nutriscore,product.url")
-            data_base.commit()
-            data_base.close()
+        data_base = mysql.connector.connect(host='localhost',user='chanelire',password='Baudelaire666!',database='OpenFoodFacts')
+        cursor = data_base.cursor() # Allows to iterate through the database
+        sql = "INSERT INTO product (name,nutriscore,url) VALUES (%s,%s,%s)"
+        values = products
+        cursor.execute(sql,values)
+        data_base.commit()
+        data_base.close()
 
