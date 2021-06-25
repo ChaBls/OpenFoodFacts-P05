e7635d7f6569a0e9feb455db1bc645ba2d3fb2b3
ChaBls <charline.builles@hotmail.fr>
2021-06-25 13:05:32 +0200

    object_list changed into tuple

--- product_manager.py
+++ product_manager.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:Utf8
+from category_manager import CategoryManager
 import mysql.connector
 
 
 class ProductManager:
     
-    def save(self, products):
+    def save(self,products):
         # The method allows the program to be connected to MariaDB database.
 
         data_base = mysql.connector.connect(host='localhost',user='chanelire',password='Baudelaire666!',database='OpenFoodFacts')
@@ -14,6 +15,11 @@ class ProductManager:
         values = products
         cursor.execute(sql,values)
         data_base.commit()
-        print(cursor.lastrowid)
+        print(cursor.lastrowid) # Test
         data_base.close()
+
+    def saving_categories(self,object_list):
+        category_manager=CategoryManager()
+        object_list=tuple(object_list)
+        category_manager.save(object_list)
 
