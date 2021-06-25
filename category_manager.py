004be0531e3e1a20801af10f98e42ced896ba943
ChaBls <charline.builles@hotmail.fr>
2021-06-25 13:05:08 +0200

    init method modified

--- category_manager.py
+++ category_manager.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:Utf8
+import mysql.connector
 
 
 class CategoryManager:
-    pass
+    
+    def save(self,products):
+        data_base = mysql.connector.connect(host='localhost',user='chanelire',password='Baudelaire666!',database='OpenFoodFacts')
+        cursor = data_base.cursor()
+        sql = "INSERT INTO category (name) VALUES (%s)"
+        values = products
+        cursor.execute(sql,values)
+        data_base.commit()
+        sql_dinstinct = "INSERT DISTINCT name FROM categories"
+        cursor.execute(sql_dinstinct)
+        print(cursor.lastrowid) # Test
+        data_base.close()
+
